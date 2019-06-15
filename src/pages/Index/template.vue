7b5560e8e372cb899d66daacb6d444d5fb15d006
Kami Xu <605464527@qq.com>
2019-06-15 23:00:52 +0800

    1

---
+++ src/pages/Index/template.vue
@@ -0,0 +1,9 @@
+<template>
+  <div id="index">
+    博客首页
+  </div>
+</template>
+
+<script src="./template.js"></script>
+
+<style src="./template.css"></style>
---
+++ src/pages/Login/template.vue
@@ -0,0 +1,9 @@
+<template>
+  <div id="login">
+    <p>登陆页面</p>
+  </div>
+</template>
+
+<script src="./template.js"></script>
+
+<style scoped lang="less" src="./template.less"></style>

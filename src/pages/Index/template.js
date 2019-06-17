8d05c0afbee7df3fe89e9d7c44f868b99e82c664
Kami Xu <605464527@qq.com>
2019-06-17 17:02:21 +0800

    api

--- src/pages/Index/template.js
+++ src/pages/Index/template.js
@@ -1,7 +1,24 @@
+import request from '@/helpers/request.js'
+window.request = request
+
+import auth from '@/api/auth.js'
+window.auth = auth
+
+import blog from '@/api/blog.js'
+window.blog = blog
+
 export default {
   data() {
     return {
       msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    onClick1() {
+      this.$message({
+        message: '恭喜你，这是一条成功消息',
+        type: 'success'
+      });
     }
   }
 }
--- src/pages/Index/template.vue
+++ src/pages/Index/template.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="index">
     博客首页
+    <div>
+      <el-button @click="onClick1" type="success">成功按钮</el-button>
+    </div>
   </div>
 </template>
 

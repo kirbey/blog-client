6909399965872f5977b8f307674c77f4f184cb0d
Kami Xu <605464527@qq.com>
2019-06-17 22:22:54 +0800

    css

---
+++ src/components/footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer>
+    <p>©email:605464527@qq.com  tel:17671173243</p>
+  </footer>
+</template>
+<style scoped>
+footer{
+  align-self: end;
+  background-color: #d7d7d7;
+  color: #666;
+  font-size: 13px;
+  padding: 10px;
+  text-align: center;
+}
+</style>
+
--- src/pages/Index/template.vue
+++ src/pages/Index/template.vue
@@ -2,7 +2,6 @@
   <div id="index">
     博客首页
     <div>
-      <el-button @click="onClick1" type="success">成功按钮</el-button>
     </div>
   </div>
 </template>

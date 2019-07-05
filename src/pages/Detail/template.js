6b55a37924753aa79c6bab7c8ef56ab3efbbdc98
Kami Xu <605464527@qq.com>
2019-07-05 17:52:17 +0800

    完善博客详情页面

--- src/pages/Detail/template.js
+++ src/pages/Detail/template.js
@@ -1,7 +1,29 @@
+import marked from 'marked'
+import blog from '@/api/blog'
+
 export default {
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      title: '',
+      rawContent: '',
+      createAt: '',
+      user: {}
+    }
+  },
+
+  created(){
+    this.blogId = this.$route.params.blogId
+    blog.getDetail({blogId: this.blogId}).then( res =>{
+      this.title = res.data.title
+      this.rawContent = res.data.content
+      this.createAt = res.data.createAt
+      this.user = res.data.user
+    })
+  },
+
+  computed: {
+    markdown(){
+      return marked(this.rawContent)
     }
   }
 }
